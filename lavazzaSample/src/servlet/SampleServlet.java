c08f1312292f30be2877b1d713ded967655afe49
bassy614 <t070134@ishibashi-no-MacBook-Air-2.local>
2015-03-07 11:40:05 +0900

    テスト

--- lavazzaSample/src/servlet/SampleServlet.java
+++ lavazzaSample/src/servlet/SampleServlet.java
@@ -17,14 +17,18 @@ public class SampleServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, 
+						 HttpServletResponse response) throws ServletException, 
+						 									  IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, 
+						  HttpServletResponse response) throws ServletException, 
+						  									   IOException {
 		// TODO Auto-generated method stub
 	}
 
